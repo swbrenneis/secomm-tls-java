a5769af21f772936d3ce62c9de22d2e8bd677af4
Steve Brenneis <steve.brenneis@secomm.org>
2020-10-31 18:32:45 -0400

    Added secure random to initialize methods

--- src/main/java/org/secomm/tls/context/TlsContext.java
+++ src/main/java/org/secomm/tls/context/TlsContext.java
@@ -1,12 +1,14 @@
 package org.secomm.tls.context;
 
+import java.security.SecureRandom;
+
 public class TlsContext {
 
-    public static TlsClientContext initializeClient() {
+    public static TlsClientContext initializeClient(SecureRandom random) {
         return new TlsClientContext();
     }
 
-    public static TlsServerContext initializeServer() {
+    public static TlsServerContext initializeServer(SecureRandom random) {
         return new TlsServerContext();
     }
 }
