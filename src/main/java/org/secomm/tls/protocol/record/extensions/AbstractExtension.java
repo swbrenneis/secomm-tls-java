70c274e710d082dae5b2f2f4bd08cb6f79d7e0ab
Steve Brenneis <swbrenneis@gmail.com>
2020-11-04 08:29:36 -0500

    Coding extensions.

--- src/main/java/org/secomm/tls/protocol/record/extensions/AbstractExtension.java
+++ src/main/java/org/secomm/tls/protocol/record/extensions/AbstractExtension.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2020 Steve Brenneis.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following
+ * conditions: The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
+ * EXPRESSOR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMEN.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+
 package org.secomm.tls.protocol.record.extensions;
 
 import org.secomm.tls.util.NumberReaderWriter;
@@ -5,6 +27,7 @@ import org.secomm.tls.util.NumberReaderWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 
 public abstract class AbstractExtension implements Extension {
 
@@ -19,7 +42,9 @@ public abstract class AbstractExtension implements Extension {
     @Override
     public void encode(OutputStream out) throws IOException {
         ByteArrayOutputStream encoded = new ByteArrayOutputStream();
-        encodeExtensionData();
+        if (extensionData == null) {
+            encodeExtensionData();
+        }
         NumberReaderWriter.writeShort(extensionType, out);
         NumberReaderWriter.writeShort((short) extensionData.length, out);
         out.write(extensionData);
@@ -27,4 +52,25 @@ public abstract class AbstractExtension implements Extension {
 
     protected abstract void encodeExtensionData() throws IOException;
 
+    protected abstract void decodeExtensionData();
+
+    @Override
+    public int decode(ByteBuffer buffer) {
+        short extensionDataLength = buffer.getShort();
+        // Some extensions with have a zero size
+        if (extensionDataLength > 0) {
+            extensionData = new byte[extensionDataLength];
+            buffer.get(extensionData);
+            decodeExtensionData();
+        }
+        return extensionDataLength + 2;
+    }
+
+    @Override
+    public short getLength() throws IOException {
+        if (extensionData == null) {
+            encodeExtensionData();
+        }
+        return (short) extensionData.length;
+    }
 }
--- src/main/java/org/secomm/tls/protocol/record/extensions/ExtensionFactory.java
+++ src/main/java/org/secomm/tls/protocol/record/extensions/ExtensionFactory.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2020 Steve Brenneis.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following
+ * conditions: The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
+ * EXPRESSOR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMEN.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+
 package org.secomm.tls.protocol.record.extensions;
 
 import java.util.ArrayList;
@@ -13,7 +35,11 @@ public class ExtensionFactory {
     }
 
     private static final Map<Short, ExtensionBuilder<?>> extensionsMap = Stream.of(new Object[][] {
-            { Extensions.SERVER_NAME_INDICATION, new ServerNameIndicationExtension.Builder() }
+            { Extensions.SERVER_NAME_INDICATION, new ServerNameIndication.Builder() },
+            { Extensions.SUPPORTED_POINT_FORMATS, new SupportedPointFormats.Builder() },
+            { Extensions.SUPPORTED_ELLIPTIC_CURVES, new SupportedEllipticCurves.Builder() },
+            { Extensions.SESSION_TICKET, new SessionTicket.Builder() },
+            { Extensions.SIGNATURE_ALGORITHMS, new SignatureAlgorithms.Builder() }
     }).collect(Collectors.toMap(e -> (Short) e[0], e -> (ExtensionBuilder<?>) e[1]));
 
     private static List<Extension> currentExtensions = new ArrayList<>();
