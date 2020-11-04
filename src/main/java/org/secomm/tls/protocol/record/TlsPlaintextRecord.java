420d1cec22008ae55c3ae79573aabaa0b30da5e6
Steve Brenneis <swbrenneis@gmail.com>
2020-11-04 08:42:18 -0500

    Exception renamed and added copyright.

--- src/main/java/org/secomm/tls/protocol/record/TlsPlaintextRecord.java
+++ src/main/java/org/secomm/tls/protocol/record/TlsPlaintextRecord.java
@@ -1,8 +1,30 @@
+/*
+ * Copyright (c) 2020 Steve Brenneis.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so, subject to the following
+ * conditions: The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND
+ * EXPRESSOR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMEN.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *
+ */
+
 package org.secomm.tls.protocol.record;
 
+import org.secomm.tls.protocol.record.extensions.InvalidExtensionTypeException;
 import org.secomm.tls.util.NumberReaderWriter;
 
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
@@ -18,7 +40,8 @@ public class TlsPlaintextRecord extends TlsRecord {
     }
 
     public void decode(Reader in)
-            throws InvalidContentType, InvalidEncodingException, InvalidHandshakeType, IOException {
+            throws InvalidContentTypeException, InvalidEncodingException, InvalidHandshakeType, IOException,
+            InvalidExtensionTypeException {
 
         byte[] headerBytes = new byte[5];
         NumberReaderWriter.readBytes(headerBytes, in);
